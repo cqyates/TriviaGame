754bc59e38d62255500f82b93aade9d83a29a739
Corey Yates <coreyyates@Coreys-MacBook-Pro.local>
2019-03-22 10:05:16 -0400

    after tutorial

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,74 +3,75 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unansweredQuestions = 0;
 var timer = 30;
+var questionIndex = 0;
 
 // create object tree
 var triviaQuestions = [
     
-    q1 = {question: "Which 2020 candidate played football in college?",
+    {question: "Which 2020 candidate played football in college?",
        multipleChoice: ["John Hickenlooper", "Joe Biden", "Julian Castro", "Corey Booker"],
        correctAnswer: 3,
-       image: url('../images/corybooker.jpg')
+       image: '../images/corybooker.jpg'
         },
-    q2 = {question: "Which candidate once ate a salad with a comb when an aide dropped her silverware?",
+    {question: "Which candidate once ate a salad with a comb when an aide dropped her silverware?",
         multipleChoice: ["Amy Klobucher", "Kirsten Gillibrand", "Kamala Harris", "Elizabeth Warren"],
         correctAnswer: 0,
-        image: url('../images/amyk.jpg')
+        image: '../images/amyk.jpg'
         },
-    q3 = {question: "Which Candidate Speaks 7 Languages?",
+    {question: "Which Candidate Speaks 7 Languages?",
         multipleChoice: ["Elizabeth Warren", "Bernie Sanders", "Pete Buttigieg", "Kamala Harris"],
         correctAnswer: 2, 
-        image: url('../images/pete-buttigieg.jpg')
+        image: '../images/pete-buttigieg.jpg'
         },
-    q4 = {question: "Which presidential candidate's working class upbringing lead her to become a leading expert on Bankruptcy Law?",
+    {question: "Which presidential candidate's working class upbringing lead her to become a leading expert on Bankruptcy Law?",
         multipleChoice: ["Kirsten Gillibrand", "Elizabeth Warren", "Amy Klobucher", "Kamala Harris"],
         correctAnswer: 1,
-        image: url('../images/elizabeth-warren.jpg')
+        image: '../images/elizabeth-warren.jpg'
         },
-    q5 = {question: "Which of the Following Candidates is NOT a lawyer?",
+    {question: "Which of the Following Candidates is NOT a lawyer?",
         multipleChoice: ["Amy Klobucher", "Kamala Harris", "John Hickenlooper", "Kirsten Gillibrand"],
         correctAnswer: 2,
-        image: url('../images/hickenlooper.jpg')
+        image: '../images/hickenlooper.jpg'
         },
-    q6 = {question: "Who has not served as a mayor?",
+    {question: "Who has not served as a mayor?",
         multipleChoice: ["Cory Booker", "John Hickenlooper", "Pete Buttigieg", "Jay Inslee",],
         correctAnswer: 3,
-        image: url('../images/jay-inslee.jpg')
+        image: '../images/jay-inslee.jpg'
         },
-    q7 = {question: "Who is the only 2020 democratic candidate to refuse to release his tax returns?",
+    {question: "Who is the only 2020 democratic candidate to refuse to release his tax returns?",
         multipleChoice: ["Kamala Harris", "Joe Biden", "Beto O'Rourke", "Bernie Sanders"],
         correctAnswer: 3,
-        image: url('../images/bernie-sanders.jpg')
+        image: '../images/bernie-sanders.jpg'
         },
-    q8 = {question: "How many years has Joe Biden served in public office?",
+    {question: "How many years has Joe Biden served in public office?",
         multipleChoice: ["35", "43", "38", "45"],
         correctAnswer: 1,
-        image: url('../images/bernie-sanders.jpg')
+        image: '../images/bernie-sanders.jpg'
         },
-    q9 = {question: "Which candidate belonged to a hack-tivist collective?",
+    {question: "Which candidate belonged to a hack-tivist collective?",
         multipleChoice: ["Elizabeth Warren", "Joe Biden", "Bernie Sanders", "Beto O'Rourke"],
         correctAnswer: 3,
-        image: url('../images/beto.jpg')
+        image: '../images/beto.jpg'
         },
-    q10 = {question: "Which candidate kept their seat on the Senate Judicary Committee after Chuck Shumer cut a deal with Republicans?",
+    {question: "Which candidate kept their seat on the Senate Judicary Committee after Chuck Shumer cut a deal with Republicans?",
         multipleChoice: ["Kirsten Gillibrand", "Kamala Harris", "Cory Booker", "Amy Klobucher"],
         correctAnswer: 1,
-        image: url('../images/kamala.jpg')
-        },
-    q11 = {question: "Who has a twin?",
+        image: '../images/kamala.jpg'
+    },
+    {question: "Who has a twin?",
         multipleChoice: ["Julian Castro", "Cory Booker", "Jay Inslee", "Bernie Sanders"],
         correctAnswer: 0,
-        image: url('../images/castro.jpg')
+        image: '../images/castro.jpg'
         },
-    q12 = {question: "Which candidate first got their seat by being appointed rather than elected?",
+    {question: "Which candidate first got their seat by being appointed rather than elected?",
         multipleChoice: ["Kamala Harris", "Kirsten Gillibrand", "Elizabeth Warren", "Cory Booker"],
         correctAnswer: 1,
-        image: url('../images/kirsten-gillibrand.jpg')
+        image: '../images/kirsten-gillibrand.jpg'
         },   
-    q13 = {question: "Which candidate would be better than Donald Trump?",
+    {question: "Which candidate would be better than Donald Trump?",
         multipleChoice: ["Amy Klobucher", "Elizabeth Warren", "Cory Booker", "Kamala Harris", "Bernie Sanders", "Joe Biden", "Kirsten Gillibrand","Julian Castro", "Beto O'Rourke", "Jay Inslee", "John Hickenlooper", "Pete Buttigieg","All of the Above"],
         correctAnswer: 12,
-        image: url('../images/vote_blue.jpg')
+        image: '../images/vote_blue.jpg'
         }     
 ];
     //hide timer at start
@@ -79,12 +80,17 @@ var triviaQuestions = [
     console.log(triviaQuestions);
     //yay works!
 
-    //This function needs to grab one question and it's associated multiple choices, then it needs to display in the question card box
-    //this is where my understanding breaks down.
+    //This function needs to grab one question and it's associated multiple choices, then it needs to display in the question card bo
     function getQuestion() {
-        triviaQuestions[0].question
-        $('')
-        console.log();
+       var currentQuestion = triviaQuestions[questionIndex].question;
+        $('#question').html(currentQuestion);
+        console.log(triviaQuestions[questionIndex].question);
+        for( var i = 0; i < triviaQuestions[questionIndex].multipleChoice.length; i++) {
+            console.log(triviaQuestions[questionIndex].multipleChoice[i]);
+            $('#multiple-choice').append("<input type='radio' name='candidate' value='" + triviaQuestions[questionIndex].multipleChoice[i] + "'>" + triviaQuestions[questionIndex].multipleChoice[i] + "<br>")
+        }
+        
+        
     }
 
     //create an on click event that starts the quiz when you click the get started button
@@ -93,11 +99,7 @@ var triviaQuestions = [
        $('#start-image').hide();
        $('#question-card').show();
        getQuestion();
-       //for loop through triviaQuestion array (inside a function called getQuestion?)
-       //display question and multiple choice answers (inside a function called displayQuestion)
-       //record the users answer
-       //compare user answer to right answer
-
+     
    })
     
     
