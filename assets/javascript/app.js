47d22335850efb4358ab767cd3e7e9fe2a50c1b7
Corey Yates <coreyyates@Coreys-MacBook-Pro.local>
2019-03-22 09:07:55 -0400

    friday update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,54 +10,67 @@ var triviaQuestions = [
     q1 = {question: "Which 2020 candidate played football in college?",
        multipleChoice: ["John Hickenlooper", "Joe Biden", "Julian Castro", "Corey Booker"],
        correctAnswer: 3,
+       image: url('../images/corybooker.jpg')
         },
     q2 = {question: "Which candidate once ate a salad with a comb when an aide dropped her silverware?",
         multipleChoice: ["Amy Klobucher", "Kirsten Gillibrand", "Kamala Harris", "Elizabeth Warren"],
         correctAnswer: 0,
+        image: url('../images/amyk.jpg')
         },
     q3 = {question: "Which Candidate Speaks 7 Languages?",
         multipleChoice: ["Elizabeth Warren", "Bernie Sanders", "Pete Buttigieg", "Kamala Harris"],
         correctAnswer: 2, 
+        image: url('../images/pete-buttigieg.jpg')
         },
     q4 = {question: "Which presidential candidate's working class upbringing lead her to become a leading expert on Bankruptcy Law?",
         multipleChoice: ["Kirsten Gillibrand", "Elizabeth Warren", "Amy Klobucher", "Kamala Harris"],
         correctAnswer: 1,
+        image: url('../images/elizabeth-warren.jpg')
         },
     q5 = {question: "Which of the Following Candidates is NOT a lawyer?",
         multipleChoice: ["Amy Klobucher", "Kamala Harris", "John Hickenlooper", "Kirsten Gillibrand"],
         correctAnswer: 2,
+        image: url('../images/hickenlooper.jpg')
         },
     q6 = {question: "Who has not served as a mayor?",
         multipleChoice: ["Cory Booker", "John Hickenlooper", "Pete Buttigieg", "Jay Inslee",],
         correctAnswer: 3,
+        image: url('../images/jay-inslee.jpg')
         },
     q7 = {question: "Who is the only 2020 democratic candidate to refuse to release his tax returns?",
         multipleChoice: ["Kamala Harris", "Joe Biden", "Beto O'Rourke", "Bernie Sanders"],
         correctAnswer: 3,
+        image: url('../images/bernie-sanders.jpg')
         },
     q8 = {question: "How many years has Joe Biden served in public office?",
         multipleChoice: ["35", "43", "38", "45"],
         correctAnswer: 1,
+        image: url('../images/bernie-sanders.jpg')
         },
     q9 = {question: "Which candidate belonged to a hack-tivist collective?",
         multipleChoice: ["Elizabeth Warren", "Joe Biden", "Bernie Sanders", "Beto O'Rourke"],
         correctAnswer: 3,
+        image: url('../images/beto.jpg')
         },
     q10 = {question: "Which candidate kept their seat on the Senate Judicary Committee after Chuck Shumer cut a deal with Republicans?",
         multipleChoice: ["Kirsten Gillibrand", "Kamala Harris", "Cory Booker", "Amy Klobucher"],
         correctAnswer: 1,
+        image: url('../images/kamala.jpg')
         },
     q11 = {question: "Who has a twin?",
         multipleChoice: ["Julian Castro", "Cory Booker", "Jay Inslee", "Bernie Sanders"],
         correctAnswer: 0,
+        image: url('../images/castro.jpg')
         },
     q12 = {question: "Which candidate first got their seat by being appointed rather than elected?",
         multipleChoice: ["Kamala Harris", "Kirsten Gillibrand", "Elizabeth Warren", "Cory Booker"],
         correctAnswer: 1,
+        image: url('../images/kirsten-gillibrand.jpg')
         },   
     q13 = {question: "Which candidate would be better than Donald Trump?",
         multipleChoice: ["Amy Klobucher", "Elizabeth Warren", "Cory Booker", "Kamala Harris", "Bernie Sanders", "Joe Biden", "Kirsten Gillibrand","Julian Castro", "Beto O'Rourke", "Jay Inslee", "John Hickenlooper", "Pete Buttigieg","All of the Above"],
         correctAnswer: 12,
+        image: url('../images/vote_blue.jpg')
         }     
 ];
     //hide timer at start
@@ -67,8 +80,10 @@ var triviaQuestions = [
     //yay works!
 
     //This function needs to grab one question and it's associated multiple choices, then it needs to display in the question card box
+    //this is where my understanding breaks down.
     function getQuestion() {
-        for (var i = 0; i > triviaQuestions.length; i++);
+        triviaQuestions[0].question
+        $('')
         console.log();
     }
 
