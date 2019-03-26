2d935621f8bb5d1c99bfdc060fb5e6222301e9b1
Corey Yates <coreyyates@Coreys-MacBook-Pro.local>
2019-03-26 13:32:08 -0400

    images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,67 +3,67 @@ var triviaQuestions = [
     {question: "Which 2020 candidate played football in college?",
        multipleChoice: ["John Hickenlooper", "Joe Biden", "Julian Castro", "Cory Booker"],
        correctAnswer: "Cory Booker",
-       image: '../images/corybooker.jpg'
+       image: 'assets/images/corybooker.jpg'
         },
     {question: "Which candidate once ate a salad with a comb when an aide dropped her silverware?",
         multipleChoice: ["Amy Klobucher", "Kirsten Gillibrand", "Kamala Harris", "Elizabeth Warren"],
         correctAnswer: "Amy Klobucher",
-        image: '../images/amyk.jpg'
+        image: 'assets/images/amyk.jpg'
         },
     {question: "Which Candidate Speaks 7 Languages?",
         multipleChoice: ["Elizabeth Warren", "Bernie Sanders", "Pete Buttigieg", "Kamala Harris"],
         correctAnswer: "Pete Buttigieg",
-        image: '../images/pete-buttigieg.jpg'
+        image: 'assets/images/pete-buttigieg.jpg'
         },
     {question: "Which presidential candidate's working class upbringing lead her to become a leading expert on Bankruptcy Law?",
         multipleChoice: ["Kirsten Gillibrand", "Elizabeth Warren", "Amy Klobucher", "Kamala Harris"],
         correctAnswer: "Elizabeth Warren",
-        image: '../images/elizabeth-warren.jpg'
+        image: 'assets/images/elizabeth-warren.jpg'
         },
     {question: "Which of the Following Candidates is NOT a lawyer?",
         multipleChoice: ["Amy Klobucher", "Kamala Harris", "John Hickenlooper", "Kirsten Gillibrand"],
         correctAnswer: "John Hickenlooper",
-        image: '../images/hickenlooper.jpg'
+        image: 'assets/images/hickenlooper.jpg'
         },
     {question: "Who has not served as a mayor?",
         multipleChoice: ["Cory Booker", "John Hickenlooper", "Pete Buttigieg", "Jay Inslee"],
         correctAnswer: "Jay Inslee",
-        image: '../images/jay-inslee.jpg'
+        image: 'assets/images/jay-inslee.jpg'
         },
     {question: "Who is the only 2020 democratic candidate to refuse to release his tax returns?",
         multipleChoice: ["Kamala Harris", "Joe Biden", "Beto O'Rourke", "Bernie Sanders"],
         correctAnswer: "Bernie Sanders",
-        image: '../images/bernie-sanders.jpg'
+        image: 'assets/images/bernie-sanders.jpg'
         },
     {question: "How many years has Joe Biden served in public office?",
         multipleChoice: ["35", "43", "38", "45"],
         correctAnswer: "43",
-        image: '../images/bernie-sanders.jpg'
+        image: 'assets/images/bernie-sanders.jpg'
         },
     {question: "Which candidate belonged to a hack-tivist collective?",
         multipleChoice: ["Elizabeth Warren", "Joe Biden", "Bernie Sanders", "Beto O'Rourke"],
         correctAnswer: "Beto O'Rourke",
-        image: '../images/beto.jpg'
+        image: 'assets/images/beto.jpg'
         },
     {question: "Which candidate kept their seat on the Senate Judicary Committee after Chuck Schumer cut a deal with Republicans?",
         multipleChoice: ["Kirsten Gillibrand", "Kamala Harris", "Cory Booker", "Amy Klobucher"],
         correctAnswer: "Kamala Harris",
-        image: '../images/kamala.jpg'
+        image: 'assets/images/kamala.jpg'
     },
     {question: "Who has a twin?",
         multipleChoice: ["Julian Castro", "Cory Booker", "Jay Inslee", "Bernie Sanders"],
         correctAnswer: "Julian Castro",
-        image: '../images/castro.jpg'
+        image: 'assets/images/castro.jpg'
         },
     {question: "Which candidate first got their seat by being appointed rather than elected?",
         multipleChoice: ["Kamala Harris", "Kirsten Gillibrand", "Elizabeth Warren", "Cory Booker"],
         correctAnswer: "Kirsten Gillibrand",
-        image: '../images/kirsten-gillibrand.jpg'
+        image: 'assets/images/kirsten-gillibrand.jpg'
         },   
     {question: "Which candidate would be better than Donald Trump?",
         multipleChoice: ["Amy Klobucher", "Elizabeth Warren", "Cory Booker", "Kamala Harris", "Bernie Sanders", "Joe Biden", "Kirsten Gillibrand","Julian Castro", "Beto O'Rourke", "Jay Inslee", "John Hickenlooper", "Pete Buttigieg", "All of the Above"],
         correctAnswer: "All of the Above",
-        image: '../images/vote_blue.jpg'
+        image: 'assets/images/vote_blue.jpg'
         }
 ];
 
@@ -90,9 +90,18 @@ function buildQuestionCard (){
 
 }
 
-//This Function Builds the Answer Card
 function buildAnswerCard (){
-    $()
+    var candidateImage = $('<img>');
+    $('#answer-card').show();
+
+    var imageUrl = triviaQuestions[Q].image;
+
+          //this puts the link and sourcse together
+    candidateImage.attr("src", imageUrl);
+    candidateImage.attr("alt", "candidate image");
+
+          //this shows the images before the one in front.
+          $("#images").append(candidateImage);
 }
 
 
@@ -118,8 +127,6 @@ function buildAnswerCard (){
             $('#multiple-choice').append("<input type='radio' name='candidate' value='" + triviaQuestions[Q].multipleChoice[i] + "'>" + triviaQuestions[Q].multipleChoice[i] + "<br>")
             }
         buildQuestionCard();
-     
-
     }
 
     function checkAnswer() {
@@ -127,6 +134,7 @@ function buildAnswerCard (){
             correctAnswers++;
             unansweredQuestions--;
             Q++;
+            buildAnswerCard();
             $('#question-card').hide();
             $('#question').empty();
             $('#multiple-choice').empty();
@@ -137,6 +145,7 @@ function buildAnswerCard (){
             incorrectAnswers++;
             unansweredQuestions--;
             Q++;
+            buildAnswerCard();
             $('#question-card').hide();
             $('#question').empty();
             $('#multiple-choice').empty();
